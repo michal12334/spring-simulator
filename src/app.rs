07c0d300bb2a2c93354208217c52f53cfd83d50a
Michal Boguszewski <michal1842001@gmail.com>
2024-10-10 19:16:35 +0200

    Create window

---
+++ src/app.rs
@@ -0,0 +1,13 @@
+pub struct App {}
+
+impl App {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use app::App;
+
+pub mod app;
+
+fn main() -> eframe::Result {
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Spring simulation",
+        native_options,
+        Box::new(|_| Ok(Box::new(App::new()))),
+    )
 }
