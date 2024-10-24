55d38b937db06b87fda77eaa3578e178decb7742
Michal Boguszewski <michal1842001@gmail.com>
2024-10-24 20:52:11 +0200

    Make window bigger

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ pub mod trajectory_plot;
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
+            .with_inner_size([1200.0, 750.0])
+            .with_min_inner_size([800.0, 700.0]),
         ..Default::default()
     };
     eframe::run_native(
